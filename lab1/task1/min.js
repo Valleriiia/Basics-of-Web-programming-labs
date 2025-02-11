0b5940609f0f9093c58033b4b6eb224889ea27aa
Valleriiia <valeria.nepomiashcha@gmail.com>
2025-02-11 17:39:49 +0100

    base js

---
+++ lab1/task1/min.js
@@ -0,0 +1,76 @@
+const hydrogenRInput = document.querySelector("#hydrogen-r");
+const carbonRInput = document.querySelector("#carbon-r");
+const sulfurRInput = document.querySelector("#sulfur-r");
+const nitrogenRInput = document.querySelector("#nitrogen-r");
+const oxygenRInput = document.querySelector("#oxygen-r");
+const wetRInput = document.querySelector("#wet-r");
+const ashRInput = document.querySelector("#ash-r");
+const btn = document.querySelector("#submit-btn");
+const output = document.querySelector("#output");
+
+function isCorrectInput() {
+    return +hydrogenRInput.value + +carbonRInput.value + +sulfurRInput.value + +nitrogenRInput.value + +oxygenRInput.value + +wetRInput.value + +ashRInput.value == 100;
+}
+
+function calculate(e) {
+    e.preventDefault();
+
+    if (!isCorrectInput()) {
+        alert("Please enter a value for each element that adds up to 100%");
+        return;
+    }
+    const hydrogenR = +hydrogenRInput.value;
+    const carbonR = +carbonRInput.value;
+    const sulfurR = +sulfurRInput.value;
+    const nitrogenR = +nitrogenRInput.value;
+    const oxygenR = +oxygenRInput.value;
+    const wetR = +wetRInput.value;
+    const ashR = +ashRInput.value;
+
+    let coefRC = +(100 / (100 - wetR)).toFixed(2);
+    let coefRG = +(100 / (100 - wetR - ashR)).toFixed(2);
+
+    let hydrogenC = +(hydrogenR * coefRC).toFixed(2);
+    let carbonC = +(carbonR * coefRC).toFixed(2);
+    let sulfurC = +(sulfurR * coefRC).toFixed(2);
+    let nitrogenC = +(nitrogenR * coefRC).toFixed(3);
+    let oxygenC = +(oxygenR * coefRC).toFixed(2);
+    let ashC = +(ashR * coefRC).toFixed(2);
+
+    let hydrogenG = +(hydrogenR * coefRG).toFixed(2);
+    let carbonG = +(carbonR * coefRG).toFixed(2);
+    let sulfurG = +(sulfurR * coefRG).toFixed(2);
+    let nitrogenG = +(nitrogenR * coefRG).toFixed(3);
+    let oxygenG = +(oxygenR * coefRG).toFixed(2);
+
+    let warmthR = +((339 * carbonR + 1030 * hydrogenR - 108.8 * (oxygenR - sulfurR) - 25 * wetR) / 1000).toFixed(4);
+    let warmthC = +((warmthR + 0.025 * wetR) * coefRC).toFixed(2);
+    let warmthG = +((warmthR + 0.025 * wetR) * coefRG).toFixed(2);
+
+    output.innerHTML = `
+        <p>Для палива з компонентним складом: H<sup>р</sup>=${hydrogenR}%; C<sup>р</sup>=${carbonR}%; S<sup>р</sup>=${sulfurR}%; N<sup>р</sup>=${nitrogenR}%; O<sup>р</sup>=${oxygenR}%; W<sup>р</sup>=${wetR}%; A<sup>р</sup>=${ashR}%:</p>
+        <h2>Коефіцієнти переходу</h2>
+        <p>Від робочої до сухої маси: ${coefRC}</p>
+        <p>Від робочої до горючої маси: ${coefRG}</p>
+        <h2>Склад сухої маси палива</h2>
+        <p>Водень, H<sup>с</sup>: ${hydrogenC}%</p>
+        <p>Вуглець, C<sup>с</sup>: ${carbonC}%</p>
+        <p>Сірка, S<sup>с</sup>: ${sulfurC}%</p>
+        <p>Азот, N<sup>с</sup>: ${nitrogenC}%</p>
+        <p>Кисень, O<sup>с</sup>: ${oxygenC}%</p>
+        <p>Зола, A<sup>с</sup>: ${ashC}%</p>
+        <h2>Склад горючої маси палива</h2>
+        <p>Водень, H<sup>г</sup>: ${hydrogenG}%</p>
+        <p>Вуглець, C<sup>г</sup>: ${carbonG}%</p>
+        <p>Сірка, S<sup>г</sup>: ${sulfurG}%</p>
+        <p>Азот, N<sup>г</sup>: ${nitrogenG}%</p>
+        <p>Кисень, O<sup>г</sup>: ${oxygenG}%</p>
+        <h2>Теплота згоряння</h2>
+        <p>Для робочої маси: ${warmthR} МДж/к</p>
+        <p>Для сухої маси: ${warmthC} МДж/к</p>
+        <p>Для горючої маси: ${warmthG} МДж/к</p>
+
+    `;
+}
+
+btn.addEventListener("click", calculate);
